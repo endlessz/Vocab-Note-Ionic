e2ccdfc73fb1d345172fcd134a3e6dc514189b13
Wasut <wasut_pao@hotmail.com>
2016-12-27 21:28:04 +0700

    delete unused variable, refactor paremeter to form data, delete subscribe

--- src/app/services/authen.service.ts
+++ src/app/services/authen.service.ts
@@ -1,33 +1,27 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/Rx';
 
 @Injectable()
 export class AuthenService {
 	http: any
 	baseUrl: string
-	data: any
 
 	constructor(http: Http){
 		this.http = http
 		this.baseUrl = "http://vocabnote.herokuapp.com/api/v1/"
-		this.data = {}
-
 	}
 
 	signIn(email, password){
 		console.log("Sign In " + email);
 	}
 
-	signUp(email, password){
-		console.log("Sign Up! " + email + " " + password)
-		var data = JSON.stringify({email: email, password: password});
+	signUp(formValue){
+		let data = JSON.stringify(formValue);
+		let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
 
-		this.http.post(this.baseUrl + "register", data)  
-				 .subscribe(data => {
-		         	this.data.response = data._body;
-		         }, error => {
-		            console.log("Error!");
-		         });
+		return this.http.post(this.baseUrl + "register", data, options)
+		      			.map(res => res.json())  
 	}
 }
