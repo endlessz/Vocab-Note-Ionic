f378ba06bd3511a85ed965566209efca9f082bb3
Wasut <wasut_pao@hotmail.com>
2016-12-27 22:47:19 +0700

    add ngOnInit to call getVocabs and kick to sign in page if unauthen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
-
+import { VocabService } from '../../app/services/vocab.service';
 import { NavController } from 'ionic-angular';
+import { SigninPage } from '../signin/signin';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  vocabs: any
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, private vocabService: VocabService) {
+  }
+
+  ngOnInit(){
+  	this.getVocabs()
+  }
+
+  getVocabs(){
+  	this.vocabService.getVocabs().subscribe(response => {
+  		console.log(response)
+  		this.vocabs = response.data
+  	}, error => {
+  		if(error.status == 401){
+  			console.log("Unauthen")
+  			this.navCtrl.setRoot(SigninPage)
+  		}
+  	})
   }
 
 }
