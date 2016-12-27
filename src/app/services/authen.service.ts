c63ce7881dfb699e4267e568040e6b17e1a52e4b
Wasut <wasut_pao@hotmail.com>
2016-12-27 22:21:05 +0700

    refactor headers and option to constructor, add signIn service

--- src/app/services/authen.service.ts
+++ src/app/services/authen.service.ts
@@ -6,22 +6,28 @@ import 'rxjs/Rx';
 export class AuthenService {
 	http: any
 	baseUrl: string
+	headers: any
+	options: any
 
 	constructor(http: Http){
 		this.http = http
 		this.baseUrl = "http://vocabnote.herokuapp.com/api/v1/"
+		this.headers = new Headers({ 'Content-Type': 'application/json' });
+		this.options = new RequestOptions({ headers: this.headers });
 	}
 
 	signIn(email, password){
-		console.log("Sign In " + email);
+		let data = JSON.stringify({email: email, password: password});
+
+		return this.http.post(this.baseUrl + "login", data, this.options)
+		      			.map(res => res.json())  
 	}
 
 	signUp(formValue){
 		let data = JSON.stringify(formValue);
-		let headers = new Headers({ 'Content-Type': 'application/json' });
-        let options = new RequestOptions({ headers: headers });
 
-		return this.http.post(this.baseUrl + "register", data, options)
+
+		return this.http.post(this.baseUrl + "register", data, this.options)
 		      			.map(res => res.json())  
 	}
 }
