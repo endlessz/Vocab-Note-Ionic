150fef5d02e5d4df43d27c1726dfe20e94f6cb39
Wasut <wasut_pao@hotmail.com>
2016-12-27 22:21:58 +0700

    delete mock signin use real authenService

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -2,18 +2,30 @@ import { Component } from '@angular/core';
 import { AuthenService } from '../../app/services/authen.service';
 import { NavController } from 'ionic-angular';
 import { SignupPage } from '../signup/signup';
+import { HomePage } from '../home/home';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'page-signin',
   templateUrl: 'signin.html'
 })
 export class SigninPage {
+  email: string
+  password: string
+
+  error: any
 
   constructor(public navCtrl: NavController, private authenService: AuthenService) {
+    this.email = ''
+    this.password = ''
   }
 
   signIn(){
-  	this.authenService.signIn("test", "test")
+  	this.authenService.signIn(this.email, this.password).subscribe(response => {
+      this.navCtrl.push(HomePage)
+    }, error => {  
+        this.error = "Invalid email or password"
+    })
   }
 
   goSignUp(){
