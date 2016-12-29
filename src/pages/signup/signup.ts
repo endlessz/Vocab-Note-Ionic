a75ca3522bccee45b581de2aa1e30debe3332bbe
Wasut <wasut_pao@hotmail.com>
2016-12-29 19:14:44 +0700

    remove unused import

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthenService } from '../../app/services/authen.service';
 import { ToastService } from '../../app/services/toast.service';
-import { NavController, ToastController } from 'ionic-angular';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { NavController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserValidator } from '../../app/validators/user.validator';
 
 @Component({
